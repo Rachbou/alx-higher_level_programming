525aae403d35287ed0c9ba2fd76caa790c015576
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-01-18 10:51:35 -0800

    0x02. Python - import & modules : mandatory

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import calculator_1 as calculator 
 if __name__ == "__main__":
+    import calculator_1 as calculator
     a = 10
     b = 5
     print(f"{a} + {b} = {calculator.add(a, b)}")
