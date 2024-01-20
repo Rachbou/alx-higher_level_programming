74bbcd9a151a7fd68fe7c2d72156c06a9f707df0
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-01-20 03:10:30 -0800

    0x03. Python - Data Structures: Lists, Tuples : #advanced

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - Prints some basic info about Python lists.
+ * @p: A pointer the Python Object.
+ *
+ * Return: a pointer to the new node on success.
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size, alloc, i;
+	PyObject *PyObj;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+
+		PyObj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(PyObj)->tp_name);
+	}
+}
