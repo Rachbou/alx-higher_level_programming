3ff97a4ce3b75b8c14e165cb93b5e3599ed28889
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-02-05 08:52:24 -0800

    0x05. Python - Exceptions : mandatory

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end='')
-        print('')
+        print()
+        return(x)
     except IndexError:
-        print('')
+        print()
         return(i)
-    return(x)
