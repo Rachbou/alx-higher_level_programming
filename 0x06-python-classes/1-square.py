0d53d23a2c6d316ee31a5363bf89dfe904da687e
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-02-06 12:30:47 -0800

    0x06. Python - Classes and Objects : mandatory

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+# 1-square.py
+# Rachid BOULMANI
+"""Define a class Square."""
+
+
 class Square:
     """
     a class Square that defines a square by: (based on 0-square.py)
     Private instance attribute: size
-    Instantiation with size (no type/value verification)
     """
+
     def __init__(self, size):
+        """Instantiation with size (no type/value verification)"""
         self.__size = size
