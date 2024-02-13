215be55f83d90a724d11b9e647de23bcb266c816
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-02-13 10:02:37 -0800

    0x09. Python - Everything is object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 101-locked_class.py
+# Rachid BOULMANI
+"""
+a class LockedClass with no class or object attribute,
+"""
+
+
+class LockedClass:
+    """a class LockedClass with no class or object attribute,
+    that prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name
+    """
+
+    __slots__ = 'first_name'
