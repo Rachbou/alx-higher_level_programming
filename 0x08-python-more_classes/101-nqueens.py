677cfb1671274265b5555df4cc112923b4c9d694
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-02-12 22:31:44 -0800

    0x08. Python - More Classes and Objects : #Advanced

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -109,10 +109,12 @@ if len(sys.argv) != 2:
     print("Usage: nqueens N")
     sys.exit(1)
 
-N = sys.argv[1]
-if not isinstance(N, int):
+Try:
+    N = int(sys.argv[1])
+except Exception as e:
     print("N must be a number")
     sys.exit(1)
+
 if N < 4:
     print("N must be at least 4")
     sys.exit(1)
