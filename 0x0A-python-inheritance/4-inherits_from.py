69b7199a08ffc9405a26e1743a94b12b5d3a6b14
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-02-19 11:56:19 -0800

    4. Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -16,5 +16,6 @@ def inherits_from(obj, a_class):
     """
 
     if type(obj) != a_class:
-        return (isinstance(obj, a_class))
-    return (False)
+        if isinstance(obj, a_class):
+        return True
+    return False
