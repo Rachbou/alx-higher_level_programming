37f13571591b88de0225ae578acbe68d7b8d7dc9
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-02-20 09:19:37 -0800

    0x0B. Python - Input/Output : mandatory 0 to 7

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,6 +11,5 @@ def read_file(filename=""):
     a function that reads a text file (UTF8)
     and prints it to stdout
     """
-    with open(filename, 'r', encoding="utf-8") as file:
-        data = file.read()
-    print(data)
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
