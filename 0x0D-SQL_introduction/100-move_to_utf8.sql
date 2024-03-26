2c35e8858e680880ee1c5b99bd27af0d9f1a8d56
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-03-26 13:28:22 -0700

    #advanced

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
--- Converts the entire database hbtn_0c_0 to UTF8.
-USE hbtn_0c_0 ALTER TABLE first_table
+-- Converts Field name in Table first_table from Database hbtn_0c_0 to UTF8.
+USE hbtn_0c_0 ALTER TABLE first_table MODIFY name
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
